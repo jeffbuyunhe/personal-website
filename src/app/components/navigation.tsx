38f6d3bd96ec03e5b5eed1c0e6b23de28f42daf7
Jeff He <jeffbuyunhe@hotmail.com>
2024-05-03 23:59:21 -0400

    fixed bug with keys

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -7,8 +7,8 @@ import { useState } from "react";
 
 export function DesktopNavigation() {
     return <ul className="hidden md:flex mt-8 mb-4 mx-8">{NAVIGATION_DATA.map((data) =>
-        <div className="mx-8">
-            <DesktopNavigationItem key={data.name} {...data} />
+        <div className="mx-8" key={data.name}>
+            <DesktopNavigationItem {...data} />
         </div>
     )}
     </ul>
@@ -22,8 +22,8 @@ export function MobileNavigation() {
             onClick={() => setDropDownOpen(!dropDownOpen)}>
             <FaBars /></button>
         <ul className={`${dropDownOpen ? 'block' : 'hidden'} bg-gray-100 rounded-md`}>{NAVIGATION_DATA.map((data) =>
-            <li className=" hover:bg-gray-200 hover:rounded-md active:bg-gray-300 p-3">
-                <MobileNavigationItem key={data.name} {...data} />
+            <li className=" hover:bg-gray-200 hover:rounded-md active:bg-gray-300 p-3" key={data.name}>
+                <MobileNavigationItem {...data} />
             </li>
         )}
         </ul>
