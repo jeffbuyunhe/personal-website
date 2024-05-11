815fff2d32282aab0d4a9673bdc35743932356f3
Jeff He <jeffbuyunhe@hotmail.com>
2024-05-11 04:11:00 -0400

    added name and hobbies + adjusted margins

--- src/app/components/Portrait/portrait.tsx
+++ src/app/components/Portrait/portrait.tsx
@@ -1,10 +1,25 @@
+'use client'
+
 import Image from "next/image"
+import { Typewriter } from "react-simple-typewriter"
 
 export default function Portrait() {
-    return <Image
-        className="rounded-[50%] border-pink-800 border-4 ml-6"
-        src="/portrait.png"
-        width={500}
-        height={500}
-        alt="Picture of Jeff He" />
+    return <>
+        <Image
+            className="rounded-[50%] border-pink-800 border-4 ml-6 my-8"
+            src="/portrait.png"
+            width={450}
+            height={450}
+            alt="Picture of Jeff He"
+            priority={true} />
+        <div className=" flex w-[450px] m-6 justify-center text-3xl">
+            <p className="font-semibold">Jeff He</p>
+            <Typewriter
+                words={[', Tech Enthusiast', ', Vinyl Junkie', ', Watch Collector', ', Chess Player']}
+                loop={0}
+                typeSpeed={100}
+                deleteSpeed={100}
+            />
+        </div>
+    </>
 }
