fad0fd6da0eb826a28a373836ea412a385d27506
Jeff He <jeffbuyunhe@hotmail.com>
2024-06-10 01:11:16 -0400

    fixed bug with services

--- frontend/src/app/services/email-service.ts
+++ frontend/src/app/services/email-service.ts
@@ -2,12 +2,12 @@ import { ENDPOINTS } from "./endpoints";
 import { FormInput } from "@/app/components/Contact/form";
 
 const sendEmail = (data: FormInput) => {
-
     return fetch(ENDPOINTS.EMAIL_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name: data.name, email: data.email, message: data.message }),
-    }).then((res) => res.json());
+    }).then((res) => res.json())
+        .catch(() => { return { error: "Could not reach server, message could not be sent." } });
 };
 
 const emailServices = { sendEmail };
