1b7728ad9dd099c0e1b0cbaf2015ad5816807568
Jeff He <jeffbuyunhe@hotmail.com>
2024-10-09 03:28:19 -0400

    removed dummy data

--- frontend/src/app/arcade/constants.ts
+++ frontend/src/app/arcade/constants.ts
@@ -3,10 +3,6 @@ import { GameCardData } from "./types";
 export const CARD_DATA: GameCardData[] = [
     {
         text: 'Toronto Record Store Scraper', link: '/arcade/trss', img: '/arcade/trss.png',
-        gitLink: 'https://github.com/jeffbuyunhe/toronto-record-store-scraper', comingSoon: false
-    },
-    {
-        text: 'Toronto Record Store Scraper', link: '/arcade/trss', img: '/arcade/trss.png',
         gitLink: 'https://github.com/jeffbuyunhe/toronto-record-store-scraper', comingSoon: true
     },
 ];
--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,11 +1,14 @@
 import { CardData, CenterText, Navigation } from "./types";
 
-export const NAVIGATION_DATA: Navigation[] = [{ name: 'Home', link: '/', active: true }, { name: 'Arcade', link: '/arcade', active: true }];
+export const NAVIGATION_DATA: Navigation[] = [
+    { name: 'Home', link: '/', active: true },
+    { name: 'Arcade', link: '/arcade', active: true }];
 
 export const CENTER_TEXT: CenterText[] = [
     { text: 'FULLSTACK DEVELOPER', img: 'fullstack-developer', imgHovered: 'fullstack-developer' },
     { text: 'QA ENGINEER', img: 'qa-engineer', imgHovered: 'qa-engineer' },
-    { text: 'UI/UX HOBBYIST', img: 'ui/ux-hobbyist', imgHovered: 'ui/ux-hobbyist' }];
+    { text: 'UI/UX HOBBYIST', img: 'ui/ux-hobbyist', imgHovered: 'ui/ux-hobbyist' },
+];
 
 export const GITHUB_LINK: string = "https://github.com/jeffbuyunhe";
 
