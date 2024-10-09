088bd3c7b21eacd0fa69f21a4918a2bdc379803f
Jeff He <jeffbuyunhe@hotmail.com>
2024-10-09 03:17:47 -0400

    update deploy script + finished arcade library

--- frontend/src/app/arcade/components/Cards/GameCard.tsx
+++ frontend/src/app/arcade/components/Cards/GameCard.tsx
@@ -8,18 +8,17 @@ import { FaGithub } from "react-icons/fa6";
 import { navigateArcade } from "../../utils";
 
 export function GameCard({ text, img, link, gitLink }: GameCardData) {
-    return <Card shadow="md" className="m-1 grayscale-[0.3] hover:grayscale-0 hover:scale-[0.99]" isPressable
+    return <Card shadow="md" className="m-1 grayscale-[0.3] hover:grayscale-0 hover:scale-[0.99] min-w-[248px] sm:min-w-[320px] h-[240px]" isPressable
         onPress={() => { navigateArcade(link) }}>
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
             <p className="md:text-lg mx-auto">{text}</p>
         </CardHeader>
-        <CardBody className="overflow-visible p-0">
+        <CardBody className="overflow-hidden justify-center px-3 py-1">
             <Image
-                className="mt-2 mx-3 w-[160px] h-[80px] md:w-[280px] md:h-[140px] object-cover"
+                className="w-[248px] sm:w-[320px] h-[162px] object-cover"
                 radius="lg"
                 alt={text}
-                src={img}
-            />
+                src={img} />
         </CardBody>
         <CardFooter>
             <Link href={gitLink} className="text-2xl text-gray-600 hover:text-black"><FaGithub /></Link>
@@ -28,17 +27,16 @@ export function GameCard({ text, img, link, gitLink }: GameCardData) {
 }
 
 export function ComingSoonGameCard({ text, img }: GameCardData) {
-    return <Card shadow="md" className="m-1 grayscale-[0.8] bg-gray-100">
+    return <Card shadow="md" className="m-1 grayscale-[0.8] bg-gray-100 min-w-[248px] sm:min-w-[320px] h-[240px]">
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
             <p className="md:text-lg mx-auto text-gray-600">{text}</p>
         </CardHeader>
-        <CardBody className="overflow-visible p-0">
+        <CardBody className="overflow-hidden justify-center px-3 py-1">
             <Image
-                className="mt-2 mx-3 w-[160px] h-[80px] md:w-[280px] md:h-[140px] object-cover"
+                className="w-[248px] sm:w-[320px] h-[162px] object-cover"
                 radius="lg"
                 alt={text}
-                src={img}
-            />
+                src={img} />
         </CardBody>
         <CardFooter>
             <p className="text-medium text-gray-600">COMING SOON</p>
--- frontend/src/app/arcade/components/Cards/Games.tsx
+++ frontend/src/app/arcade/components/Cards/Games.tsx
@@ -6,8 +6,7 @@ export default function Games() {
         <h3 className="text-4xl sm:text-5xl md:text-6xl mx-auto mb-6 text-orange-500">Games/Tools</h3>
         <p className="col-12 content-margin text-lg sm:text-xl text-gray-500 leading-8 mb-8 text-center m-1">Various web-based games and tools I've worked on,
             no need to download or install.</p>
-        <div className="content-margin col-12 grid grid-cols-[repeat(auto-fit,minmax(100px,max-content))] 
-        md:grid-cols-[repeat(auto-fit,minmax(140px,max-content))] justify-center">
+        <div className="content-margin justify-center flex flex-wrap w-[100vw]">
             {CARD_DATA.map((data) => data.comingSoon ? <ComingSoonGameCard {...data} key={data.text} /> : <GameCard {...data} key={data.text} />)}
         </div>
     </div>
